d9c937bfcfae86f082dff3aa337be9c99f65013b
Michał Godlewski <michalgodlewski4@gmail.com>
2019-08-10 11:01:12 +0200

    cmom

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Tutaj wprowadziłem zmiany");
+        System.out.println("Tutaj wprowadzgiłem zmiany");
     }
 }
