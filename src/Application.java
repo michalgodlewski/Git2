99f7b34b7181a739f15abf93c717ce64142ad1fe
Michał Godlewski <michalgodlewski4@gmail.com>
2019-08-10 12:02:35 +0200

    Dane

--- src/Application.java
+++ src/Application.java
@@ -4,7 +4,6 @@ public class Application {
         System.out.println("Godlewski");
         System.out.println("Białystok");
         System.out.println("Aleja Solidarności");
-        System.out.println("nowy tekstll");
-        System.out.println("nowy tekstll");
+
     }
 }
