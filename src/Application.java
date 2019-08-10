82ce2efa8b5f710c81dca0721d9b2b8924e50beb
Michał Godlewski <michalgodlewski4@gmail.com>
2019-08-10 12:02:04 +0200

    nowy

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,9 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("to jest nowy branch");
-        System.out.println("nowy tekstll");
-        System.out.println("nowy tekstll");
-        System.out.println("nowy tekstll");
+        System.out.println("Michał");
+        System.out.println("Godlewski");
+        System.out.println("Białystok");
+        System.out.println("Aleja Solidarności");
         System.out.println("nowy tekstll");
         System.out.println("nowy tekstll");
     }
