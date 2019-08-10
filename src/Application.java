ae323c1c735e5e0f0e6b9c960e8787d923da0fb6
Michał Godlewski <michalgodlewski4@gmail.com>
2019-08-10 11:44:44 +0200

    nowy

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("to jest nowy branch");
-        System.out.println("nowy tekstl");
+        System.out.println("nowy tekstll");
     }
 }
